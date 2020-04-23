c4e6dcfc7b30aeaeeeb78c1916b13191052ae8fc
Campelo <flaviocampelo@hotmail.com>
2020-04-23 16:05:15 -0400

    PowerShell script to export a site to xml file

---
+++ SharePoint/ExportSiteToFile.ps1
@@ -0,0 +1,42 @@
+﻿<#
+
+.SYNOPSIS
+This is a Powershell script to export a SharePoint site.
+
+.DESCRIPTION
+The script itself will export sharepoint site to a xml file.
+
+.EXAMPLE
+.\ExportSiteToFile.ps1 [SiteUrl]
+
+.NOTES
+This script will export a site with all handlers by default.
+About handlers enum: https://docs.microsoft.com/fr-ca/dotnet/api/officedevpnp.core.framework.provisioning.model.handlers
+About ProvisioningTemplate: https://docs.microsoft.com/en-us/powershell/module/sharepoint-pnp/get-pnpprovisioningtemplate
+
+.PARAMETER SiteUrl
+Url from original site collection or sub-site. Ex: "https://MyCompany.sharepoint.com/sites/MySite"
+
+#>
+param (
+    [Parameter(Mandatory=$True, Position=1)]
+    [string] $SiteUrl
+)
+
+try{
+    #xml or pnp
+    $FileName = $($SiteUrl).TrimEnd("/").Split("/")[-1]
+    $OriginalSiteFileName = "$($FileName).xml"
+
+    Write-Host "Connecting to site '$($SiteUrl)'..." -ForegroundColor Cyan
+    Connect-PnPOnline -Url "$($SiteUrl)" -UseWebLogin
+
+    Write-Host "Exporting site's template to '$($OriginalSiteFileName)' file..." -ForegroundColor Cyan
+    #Get-PnPProvisioningTemplate -Out .\$OriginalSiteFileName -PersistBrandingFiles -ExcludeHandlers SiteSecurity, PropertyBagEntries, Navigation
+    Get-PnPProvisioningTemplate -Out .\$OriginalSiteFileName -PersistBrandingFiles -Handlers All
+}
+catch{
+}
+
+Write-Host "Disconnecting..." -ForegroundColor Cyan
+Disconnect-PnPOnline
