e83b7f27bced56dca045fb1a57b73bb5d63ec193
Campelo <flaviocampelo@hotmail.com>
2020-09-08 17:18:09 -0400

    Exporting TermSet

---
+++ SharePoint/ExportAllTermSetToXml.ps1
@@ -0,0 +1,12 @@
+﻿#Config Variables
+$AdminCenterURL = "https://xxx-admin.sharepoint.com"
+ 
+#Connect to PnP Online
+#Connect-PnPOnline -Url $AdminCenterURL -Credentials (Get-Credential)
+Connect-PnPOnline -Url $AdminCenterURL -UseWebLogin
+ 
+#Export Term Store Data to XML
+Export-PnPTermGroupToXml -Out "C:\Temp\TermStoreData.xml"
+
+
+#Read more: https://www.sharepointdiary.com/2016/12/sharepoint-online-powershell-to-export-term-store-data.html#ixzz6XU9YRCH6
---
+++ SharePoint/ExportTermSetById.ps1
@@ -0,0 +1,14 @@
+﻿#Config Variables
+$AdminCenterURL = "https://xxx-admin.sharepoint.com"
+$TermSetId="404cfd9f-8c74-4fbd-86d7-8ba39e66ddd7"
+$FilePath="C:\Temp\TermsetData.txt"
+ 
+#Connect to PnP Online
+#Connect-PnPOnline -Url $AdminCenterURL -Credentials (Get-Credential)
+Connect-PnPOnline -Url $AdminCenterURL -UseWebLogin
+ 
+#Export Term set
+Export-PnPTaxonomy -TermSetId $TermsetID -Path $FilePath
+
+
+#Read more: https://www.sharepointdiary.com/2016/12/sharepoint-online-powershell-to-export-term-set-to-csv.html#ixzz6XUEYXPEf
