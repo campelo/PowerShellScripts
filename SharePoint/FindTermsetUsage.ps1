51ecacb2a915dd18969b5b9f332f109ba2b25272
Flavio Campelo <flaviocampelo@hotmail.com>
2020-12-04 12:04:36 -0500

    Adding a script to find all sites that use a specific termset

---
+++ SharePoint/FindTermsetUsage.ps1
@@ -0,0 +1,45 @@
+﻿<#
+
+.SYNOPSIS
+Find sites are using a specific termset.
+
+.DESCRIPTION
+This script will search all sharepoint sites that is using a specific termset.
+
+.EXAMPLE
+.\FindTermsetUsage.ps1 [SiteUrl] [TermsetGuid]
+.\FindTermsetUsage.ps1 "https://MyCompany.sharepoint.com/" "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+
+.NOTES
+Source: https://www.techmikael.com/2018/05/locating-where-term-set-is-used-in.html
+
+.PARAMETER SiteUrl
+Sharepoint tenant's url. Ex: "https://MyCompany.sharepoint.com/"
+
+.PARAMETER TermsetGuid
+Termset GUID. Ex: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True, Position = 1)]
+    [string] $SiteUrl,
+    [Parameter(Mandatory = $True, Position = 2)]
+    [string] $TermsetGuid
+)
+
+try {  
+    #Connect to PNP Online
+    Write-Host "Connecting to sharepoint '$($SiteUrl)'..." -ForegroundColor Cyan
+    Connect-PnPOnline -Url $($SiteUrl) -UseWebLogin
+ 
+    #Get the List
+    Write-Host "Searching usages for termset..." -ForegroundColor Cyan
+    Submit-PnPSearchQuery -Query "$($TermsetGuid)" -CollapseSpecification "SPSiteUrl:1" -RelevantResults -SelectProperties "SPSiteUrl" | Select-Object SPSiteUrl
+}
+catch {
+    write-host "Error: $($_.Exception.Message)" -foregroundcolor Red
+}
+
+Write-Host "Disconnecting..." -ForegroundColor Cyan
+Disconnect-PnPOnline
