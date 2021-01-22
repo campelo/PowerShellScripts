f1ea1ae7c953a4c93edb9fa4941da5dc59be3e07
Flavio <flaviocampelo@hotmail.com>
2021-01-22 10:40:59 -0500

    Script to install PnP-Module

---
+++ SharePoint/_installPnP.ps1
@@ -0,0 +1,30 @@
+﻿<#
+
+.SYNOPSIS
+This script will install PnP module for Sharepoint online
+
+.DESCRIPTION
+This script will install PnP module for Sharepoint online
+
+.EXAMPLE
+.\_installPnP.ps1
+
+.NOTES
+Probably you should enable the script execution in your computer before running this script.
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+Source: https://docs.microsoft.com/en-us/powershell/sharepoint/sharepoint-pnp/sharepoint-pnp-cmdlets
+
+#>
+[CmdletBinding()]
+param()
+
+try {  
+    #Install PnP module
+    Install-Module SharePointPnPPowerShellOnline
+    #Enable script execution (Bypass or Unrestricted)
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+}
+catch {
+    write-host "Error: $($_.Exception.Message)" -foregroundcolor Red
+}
