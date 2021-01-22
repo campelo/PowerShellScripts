fa8f7d0676f3ceeee97da1349348321595c24701
Flavio <flaviocampelo@hotmail.com>
2021-01-22 18:12:35 -0500

    Script to copy SharePoint list content to another list

---
+++ SharePoint/CopyListItems.ps1
@@ -0,0 +1,67 @@
+﻿<#
+
+.SYNOPSIS
+Copy list items from a site to another.
+
+.DESCRIPTION
+Copy list items from a site to another. Simple content will be copied. This script was not made to complex content migration.
+
+.EXAMPLE
+.\ChangeListUrl.ps1 [Site1] [List1] [Site2] [List2]
+.\ChangeListUrl.ps1 "https://MyCompany.sharepoint.com/sites/SourceSiteCollection" "Source List" "https://MyCompany.sharepoint.com/sites/TargetSiteCollection" "Target List"
+
+.PARAMETER Site1
+Source site
+
+.PARAMETER List1
+Source list
+
+.PARAMETER Site2
+Target site
+
+.PARAMETER List2
+Target list. No required if target list has the same name that source list.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory, Position = 1)]
+    [string] $Site1,
+    [Parameter(Mandatory, Position = 2)]
+    [string] $List1,
+    [Parameter(Mandatory, Position = 3)]
+    [string] $Site2,
+    [Parameter(Position = 4)]
+    [string] $List2
+)
+
+try {  
+    if([string]::IsNullOrEmpty($List2)){
+        $List2 = $List1
+    }
+
+    Connect-PnPOnline -UseWebLogin -Url $($Site1)
+    $fields = Get-PnPField -List $($List1) | Where-Object { $_.CanBeDeleted }
+    $items = Get-PnPListItem -List $($List1)
+
+    Connect-PnPOnline -UseWebLogin -Url $($Site2)
+    foreach ($it in $items) {
+        $value = @{}
+        $value.Add("Title", $it['Title'])
+        foreach ($f in $fields) {
+            $k = $f.InternalName
+            $v = $it[$k]
+            if (![string]::IsNullOrEmpty($v) -and $v.GetType().ToString().StartsWith("System")) {
+                $value.Add($k, $v)   
+            } 
+        }
+        Add-PnPListItem -List $($List2) -Values $value
+    }
+}
+catch {
+    write-host "Error: $($_.Exception.Message)" -foregroundcolor Red
+}
+
+Write-Host "Disconnecting..." -ForegroundColor Cyan
+Disconnect-PnPOnline
